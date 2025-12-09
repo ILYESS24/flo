6c0cc5045bf84c1b0a6dd5c12e8ae3d6af4cb497
ILYESS24 <ilyessbenina3@gmail.com>
2025-12-09 02:53:13 +0100

    feat: Straight borders on all 4 sides of landing page - removed rounded corners

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -65,7 +65,7 @@ const LandingPage: React.FC<LandingPageProps> = ({ onStartDesigning }) => {
   };
 
   return (
-    <div className="relative min-h-screen w-full overflow-hidden border-32 border-black rounded-3xl">
+    <div className="relative min-h-screen w-full overflow-hidden border-32 border-black">
       <ShaderAnimation />
       <div className="relative z-10 w-full max-w-4xl mx-auto min-h-screen flex flex-col items-center justify-center space-y-12 px-4">
         {/* Logo + Tagline */}
